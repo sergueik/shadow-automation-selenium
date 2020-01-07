1e53b3db26e2072d3f279b88cc723845c8f70a22
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2020-01-07 00:12:31 -0500

    intermediate revison - added massively failing test exercising comparison of old and new API

--- src/test/java/io/github/sukgu/LocalFileTest.java
+++ src/test/java/io/github/sukgu/LocalFileTest.java
@@ -1,6 +1,8 @@
 package io.github.sukgu;
 
 import static java.lang.System.err;
+
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.greaterThan;
@@ -16,6 +18,10 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.ElementNotVisibleException;
+import org.openqa.selenium.JavascriptException;
+import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -115,6 +121,39 @@ public class LocalFileTest {
 		assertThat(element, notNullValue());
 	}
 
+	@Test
+	public void test5() {
+		driver.navigate().to(getPageContent("inner_html_example.html"));
+		WebElement element = driver.findElement(By.cssSelector("body > div > h3"));
+		assertThat(element, notNullValue());
+		err.println("outerHTML: " + element.getAttribute("outerHTML"));
+		err.println(String.format("Text: \"%s\"", element.getText()));
+		// TODO: assert
+		assertThat(element.getText(), is(""));
+		WebElement parent = shadow.findElement("body > div");
+		assertThat(parent, notNullValue());
+		err.println("Parent outerHTML: " + parent.getAttribute("outerHTML"));
+		err.println(String.format("Parent text(old API): \"%s\"", parent.getText()));
+		element = null;
+		element = shadow.getShadowElement(parent, "h3");
+		assertThat(element, notNullValue());
+		err.println("Got shadow element: " + element); // toString
+		err.println("outerHTML (old API): " + element.getAttribute("outerHTML"));
+		err.println("outerHTML (new API): " + shadow.getAttribute(element, "outerHTML"));
+		err.println(String.format("Text(old API): \"%s\"", element.getText()));
+		err.println("Text(new API): " + shadow.getAttribute(element, "value"));
+
+		List<WebElement> elements = shadow.getAllShadowElement(parent, "h3");
+		assertThat(elements, notNullValue());
+		assertThat(elements.size(), greaterThan(0));
+		err.println(String.format("Located %d shadow document elements:", elements.size()));
+		elements.stream().map(e -> String.format("outerHTML (new API): %s", shadow.getAttribute(e, "outerHTML")))
+				.forEach(err::println);
+		elements.stream().map(e -> String.format("outerHTML (old API): %s", e.getAttribute("outerHTML")))
+				.forEach(err::println);
+		elements.stream().map(e -> String.format("Text (old API): \"%s\"", e.getText())).forEach(err::println);
+
+	}
 	@AfterAll
 	public static void tearDownAll() {
 		driver.close();
